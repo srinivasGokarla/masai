5b5ecebda5d4d5433aa79909df567448419af7ad
srinivasGokarla <srinivasgokarla@gmail.com>
2023-01-07 18:02:35 +0530

    body

--- src/components/Masai.jsx
+++ src/components/Masai.jsx
@@ -27,8 +27,298 @@ export const Masai = () => {
   </ul>
         </div>
     </div>
+    <div className='cent'>
+        <h1>Driven by Outcomes,<br/> Fuelled by Ambitions.</h1>
+        <div>
+            <p className='lunch'>Launch your dream career</p>
+            <p className='at'>at 0 upfront fee.</p>
+
+
+<img src="	https://www.masaischool.com/img/homepage/scribble.png"  alt=""/>
+        </div>
+        <button>APPLY NOW FOR FREE</button>
+    </div>
+
+    <div className="masai-statics"> 
+            <div className="left-statics">
+                <div className="placement-rate">
+                    <img className="sunshine" src="https://www.masaischool.com/img/homepage/sunshine.png" alt=""/>
+                    <h2> <span className="yellow-color">6000+</span> Students Currently <br/>
+
+                        Enrolled <br/>
+                        
+                     <span className="yellow-color">94%</span> Placement Rate*</h2>
+                     <img  className="underline" src="https://www.masaischool.com/img/homepage/underline.svg" alt=""/>
+                        <h4>*for students who have graduated from Masai School and are Masai accredited</h4>
+
+                </div>
+            <div className="placement-bottom">
+                <div>
+                    <h2>₹7.2 LPA <br/> Average <br/>
+                        Salary</h2>
+                </div>
+                <div>
+                    <h2>₹36 LPA <br/> Highest <br/>Salary</h2>
+                </div>
+            </div>
+        </div>
+
+        <div className="right-statics">
+            <div>
+                <img src="https://www.masaischool.com/img/homepage/media.png" alt=""/>
+            </div>
+            <h2>Masai In The News</h2>
+            <button>KNOW MORE</button>
+        </div>
+        </div>
+        <br/>
+
+        <div className="skill-cards">
+            <div className="skill-heading">
+<h2>Skill. Reskill. Upskill.</h2>
+<p>No matter the stage of your career, if growth in India’s tech industry is your desired outcome, we got you covered!</p>
+</div>
+     <div className="cards-section">
+        <div className="left-card">
+            <div className="top-color">
+            </div>
+            <div className="dp">
+            <img src="https://masai-website-images.s3.ap-south-1.amazonaws.com/Downloader_1_c382f15ecf.png" alt=""/>
+            </div>
+            <h3>Masai 0NE</h3>
+            <p className="practice">&lt;Learn. Practice. Grow&gt;</p>
+             <img  className="underline" src="https://www.masaischool.com/img/homepage/underline.svg" alt=""/>
+            <div className="requirments">
+                <div>
+                <p>12TH PASS</p>
+            </div>
+            <div>
+                <p>FINAL YEAR STUDENTS OR GRADUATES</p>
+            </div>
+            </div>
+            <div className="icons-para-wrapper">
+                <img src="https://findicons.com/files/icons/2770/ios_7_icons/512/student.png" alt=""/>
+                <p>Data Analytics | Software Development</p>
+            </div>
+            <div className="icons-para-wrapper">
+                <img src="https://cdn-icons-png.flaticon.com/512/3077/3077218.png" alt=""/>
+                <p>No Prior Technical Knowledge Needed</p>
+            </div>
+            <div className="icons-para-wrapper">
+                <img src="https://im.indiatimes.in/content/2020/Jul/indian-currency-389006_1920_5f1547587ee6e.jpg?w=640&h=480&cc=1" alt=""/>
+                <p>Pay After Placement of <span className="color-blue">5 LPA or Above</span></p>
+            </div>
+    
+            <div className="bottom-red">
+                VIEW COURSES
+            </div>
+          </div>
+
+     
+          <div className="right-card">
+            <div className="top-color">
+            </div>
+            <div className="dp">
+            <img src="https://masai-website-images.s3.ap-south-1.amazonaws.com/Downloader_1_c382f15ecf.png" alt=""/>
+            </div>
+            <h3>Masai 0NE</h3>
+            <p className="practice">&lt;Learn. Practice. Grow&gt;</p>
+             <img  className="underline" src="https://www.masaischool.com/img/homepage/underline.svg" alt=""/>
+            <div className="requirments">
+            <div>
+                <p>FOR WORKING PROFESSIONALS</p>
+            </div>
+            </div>
+            <div className="icons-para-wrapper">
+                <img src="https://findicons.com/files/icons/2770/ios_7_icons/512/student.png" alt=""/>
+                <p>Data Analytics | Software Development</p>
+            </div>
+            <div className="icons-para-wrapper">
+                <img src="https://im.indiatimes.in/content/2020/Jul/indian-currency-389006_1920_5f1547587ee6e.jpg?w=640&h=480&cc=1" alt=""/>
+                <p>Level 1: Pay After You Earn <span className="color-blue">5 LPA or Above</span></p>
+            </div>
+            <div className="icons-para-wrapper">
+                <img src="https://im.indiatimes.in/content/2020/Jul/indian-currency-389006_1920_5f1547587ee6e.jpg?w=640&h=480&cc=1" alt=""/>
+                <p>Level 2: Pay After You Earn <span className="color-blue">10 LPA or Above</span></p>
+            </div>
+    
+            <div className="bottom-red">
+                VIEW COURSES
+            </div>
+          </div>
+ </div>
+</div>
+
+<br/>
+<div className="scholar-preleaf">
+    <br/>
+    <div className="logos">
+        <img src="https://www.masaischool.com/img/masai-prepleaf/masai-prepleaf-logo.svg" alt=""/>
+    </div>
+<h2>Scholar programs by Prepleaf</h2>
+<p>Masai acquired Prepleaf in 2021 to expand their placement preparation portal as a go-to no cost resource for college students. Since, the 2 companies have offered a Data Analytics course with a Pay After Placement model. The Scholar Program comes as India’s first subscription based foundational learning course
+    for <br/>
+     <span className="color-purple">1st, 2nd & pre-final year students.</span></p>
+     <button>VIEW COURSES</button>
+     <p className="last-line">You can avail scholarship upto 75%.</p>
+     <br/>
+</div>
+<br/>
+<div className="start-learning">
+    <div className="btn-red-wrap">
+<button className="learning-btn">START LEARNING</button>
+</div>
+<br/><br/><br/>
+<h2>Meet Your Instructors</h2>
+<p>With extensive industry experience, the Masai Curriculum Team is a group of expert instructors.</p>
+<div className="instructors-cards">
+    <div className="card">
+        <img src="https://masai-website-images.s3.ap-south-1.amazonaws.com/Nrupul_d3fe3b289a.jpg" alt=""/>
+        <div>
+           <p>Nrupul Dev</p>
+           <p>Co Founder & CTO</p>
+           <hr/>
+           <div className="ex-wrap">
+            <img className="ex" src="https://masai-website-images.s3.ap-south-1.amazonaws.com/ex_da_557255e025.jpeg" alt=""/>
+           </div>
+           <br/>
+        </div>
+    </div>
+    <div className="card">
+        <img src="https://masai-website-images.s3.ap-south-1.amazonaws.com/Yogesh_52e31f5560.jpg" alt=""/>
+        <div>
+           <p>Yogesh bhatt</p>
+           <p>Co Founder & CTO</p>
+           <hr/>
+           <div className="ex-wrap">
+            <img className="ex" src="https://masai-website-images.s3.ap-south-1.amazonaws.com/ex_mercuri_129d690457.svg" alt=""/>
+           </div>
+           <br/>
+        </div>
+    </div>
+    <div className="card">
+        <img src="https://masai-website-images.s3.ap-south-1.amazonaws.com/aman_fbbc8625a4.jpg" alt=""/>
+        <div>
+           <p>Nrupul Dev</p>
+           <p>Co Founder & CTO</p>
+           <hr/>
+           <div className="ex-wrap">
+            <img className="ex" src="https://masai-website-images.s3.ap-south-1.amazonaws.com/ex_exl_ab21e9bfe0.png" alt=""/>
+           </div>
+           <br/>
+        </div>
+    </div>
+    <div className="card">
+        <img src="https://masai-website-images.s3.ap-south-1.amazonaws.com/Albert_28b73517f6.jpg" alt=""/>
+        <div>
+           <p>Nrupul Dev</p>
+           <p>Co Founder & CTO</p>
+           <hr/>
+           <div className="ex-wrap">
+            <img className="ex" src="https://masai-website-images.s3.ap-south-1.amazonaws.com/ex_realized_5d3fee2677.jpeg" alt=""/>
+           </div>
+           <br/>
+        </div>
+    </div>
+    <div className="card">
+        <img src="https://masai-website-images.s3.ap-south-1.amazonaws.com/lohit_dp_98abd5839c.jpeg" alt=""/>
+        <div>
+           <p>Nrupul Dev</p>
+           <p>Co Founder & CTO</p>
+           <hr/>
+           <div className="ex-wrap">
+            <img className="ex" src="https://masai-website-images.s3.ap-south-1.amazonaws.com/ex_microsoft_80272817c6.png" alt=""/>
+           </div>
+           <br/>
+        </div>
+    </div>
+    <div className="card">
+        <img src="https://masai-website-images.s3.ap-south-1.amazonaws.com/Ritesh_Firodiya_4_8a667ac9c8.jpg" alt=""/>
+        <div>
+           <p>Nrupul Dev</p>
+           <p>Co Founder & CTO</p>
+           <hr/>
+           <div className="ex-wrap">
+            <img className="ex" src="https://masai-website-images.s3.ap-south-1.amazonaws.com/ex_swiggy_9ca86a3f78.svg" alt=""/>
+           </div>
+           <br/>
+        </div>
+    </div>
+    <div className="card">
+        <img src="https://masai-website-images.s3.ap-south-1.amazonaws.com/swanand_d399feddf5.jpeg" alt=""/>
+        <div>
+           <p>Nrupul Dev</p>
+           <p>Co Founder & CTO</p>
+           <hr/>
+           <div className="ex-wrap">
+            <img className="ex" src="https://masai-website-images.s3.ap-south-1.amazonaws.com/kalaam_f2ea778a36.png" alt=""/>
+           </div>
+           <br/>
+        </div>
+    </div>
+</div>
+<br/>
+<div className="btn-red-wrap">
+    <button className="learning-btn">SEE MORE</button>
+    </div>
+</div>
+<div className="student-stories">
+    <br/><br/><br/>
+    <h2>Thousands Trust Masai To <br/> Launch Their Career</h2>
+   <div className="students-cards">
+    <div className="card">
+        <img src="https://masai-website-images.s3.ap-south-1.amazonaws.com/Aayushi_527cd1c2a3.jpg" alt=""/>
+        <div className="student-desc">
+           <p>I got placed within 2 days after graduation! I’m happy I am part of the Masai family, not because they helped me but because of their mission to help other lost</p>
+           <div className="ex-wrap">
+            <p>Aayushi Shah</p>
+            <img className="ex" src="https://masai-website-images.s3.ap-south-1.amazonaws.com/sharechat_rect_73d95cd4e1.svg" alt=""/>
+           </div>
+           <br/>
+        </div>
+    </div>
+    <div className="card">
+        <img src="https://masai-website-images.s3.ap-south-1.amazonaws.com/Kritika_Tripathi_716c47cbbd.jpg" alt=""/>
+        <div className="student-desc">
+           <p>I got placed within 2 days after graduation! I’m happy I am part of the Masai family, not because they helped me but because of their mission to help other lost</p>
+           <div className="ex-wrap">
+            <p>Aayushi Shah</p>
+            <img className="ex" src="https://masai-website-images.s3.ap-south-1.amazonaws.com/instamojo_8763e704f7.png" alt=""/>
+           </div>
+           <br/>
+        </div>
+    </div>
+    <div className="card">
+        <img src="https://masai-website-images.s3.ap-south-1.amazonaws.com/Lokesh_Nimje_90cfa775e3.jpg" alt=""/>
+        <div className="student-desc">
+           <p>I got placed within 2 days after graduation! I’m happy I am part of the Masai family, not because they helped me but because of their mission to help other lost</p>
+           <div className="ex-wrap">
+            <p>Aayushi Shah</p>
+            <img className="ex" src="https://masai-website-images.s3.ap-south-1.amazonaws.com/MPL_7713ca3d5b.svg" alt=""/>
+           </div>
+           <br/>
+        </div>
+    </div>
+    <div className="card">
+        <img src="https://masai-website-images.s3.ap-south-1.amazonaws.com/Lalitha_9c1e4f1cab.jpg" alt=""/>
+        <div className="student-desc">
+           <p>I got placed within 2 days after graduation! I’m happy I am part of the Masai family, not because they helped me but because of their mission to help other lost</p>
+           <div className="ex-wrap">
+            <p>Aayushi Shah</p>
+            <img className="ex" src="https://masai-website-images.s3.ap-south-1.amazonaws.com/Ajio_2ba59d62cb.svg" alt=""/>
+           </div>
+           <br/>
+        </div>
+    </div>
+   </div>
+   <br/>
+   <div className="btn-red-wrap">
+    <button className="learning-btn">MORE STORIES</button>
+    </div>
+</div>
          
       </div>
   
   )
 }
+
